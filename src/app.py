8e0f989a1b52692bbd0f6dc52e155da94b8df30c
Jonathan Yeh <johnnyyeh0723@gmail.com>
2021-08-17 22:05:56 +0800

    Complete syntax check for app.py.

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
+"""Import libraries."""
 import random
 import os
 import requests
 from flask import Flask, render_template, abort, request
 
-# @TODO Import your Ingestor and MemeEngine classes
 from QuoteEngine import Ingestor, QuoteModel
 from MemeEngine import MemeGenerator
 from AIEngine import AIEngine
@@ -14,8 +14,7 @@ meme = MemeGenerator('./static')
 
 
 def setup():
-    """ Load all resources """
-
+    """Load all resources."""
     quote_files = ['./_data/DogQuotes/DogQuotesTXT.txt',
                    './_data/DogQuotes/DogQuotesDOCX.docx',
                    './_data/DogQuotes/DogQuotesPDF.pdf',
@@ -27,16 +26,11 @@ def setup():
                    './_data/MyQuotes/MyQuotesDOCX.docx',
                    './_data/MyQuotes/MyQuotesCSV.csv']
 
-    # TODO: Use the Ingestor class to parse all files in the
-    # quote_files variable
     quotes = []
     for f in quote_files:
         quotes.extend(Ingestor.parse(f))
 
     images_path = "./_data/photos/picked/"
-
-    # TODO: Use the pythons standard library os class to find all
-    # images within the images images_path directory
     imgs = []
     for root, dirs, files in os.walk(images_path):
         imgs = [os.path.join(root, name) for name in files]
@@ -49,20 +43,17 @@ quotes, imgs = setup()
 
 @app.route('/')
 def meme_rand():
-    """ Generate a random meme """
-
-    # @TODO:
-    # Use the random python standard library class to:
-    # 1. select a random image from imgs array
-    # 2. select a random quote from the quotes array
-
+    """Generate a random meme."""
     img = random.choice(imgs)
     s3_img = img.split('/')[-1]
 
     bucket = 'myawsbucket0723'
-    cat_bucket = ['neymar_1.jpg', 'neymar_2.jpg', 'neymar_3.jpeg', 'neymar_4.jpeg']
-    dog_bucket = ['xander_1.jpg', 'xander_2.jpg', 'xander_3.jpg', 'xander_4.jpg']
-    view_bucket = ['iceland_1.jpeg', 'iceland_2.jpeg', 'iceland_3.jpeg', 'iceland_4.jpeg']
+    cat_bucket = ['neymar_1.jpg', 'neymar_2.jpg',
+                  'neymar_3.jpeg', 'neymar_4.jpeg']
+    dog_bucket = ['xander_1.jpg', 'xander_2.jpg',
+                  'xander_3.jpg', 'xander_4.jpg']
+    view_bucket = ['iceland_1.jpeg', 'iceland_2.jpeg',
+                   'iceland_3.jpeg', 'iceland_4.jpeg']
 
     dog_quote_files = ['./_data/DogQuotes/DogQuotesTXT.txt',
                        './_data/DogQuotes/DogQuotesDOCX.docx',
@@ -86,7 +77,7 @@ def meme_rand():
     for f in my_quote_files:
         my_quotes.extend(Ingestor.parse(f))
 
-    cat_res = AIEngine.detect_labels(s3_img, bucket, 'Cat') 
+    cat_res = AIEngine.detect_labels(s3_img, bucket, 'Cat')
     if (s3_img in cat_bucket) and cat_res:
         quote = random.choice(cat_quotes)
     dog_res = AIEngine.detect_labels(s3_img, bucket, 'Dog')
@@ -96,33 +87,25 @@ def meme_rand():
         quote = random.choice(my_quotes)
 
     path = meme.make_meme(img, quote.body, quote.author)
-    
+
     return render_template('meme.html', path=path)
 
 
 @app.route('/create', methods=['GET'])
 def meme_form():
-    """ User input for meme information """
+    """User input for meme information."""
     return render_template('meme_form.html')
 
 
 @app.route('/create', methods=['POST'])
 def meme_post():
-    """ Create a user defined meme """
-
-    # @TODO:
-    # 1. Use requests to save the image from the image_url
-    #    form param to a temp local file.
-    # 2. Use the meme object to generate a meme using this temp
-    #    file and the body and author form paramaters.
-    # 3. Remove the temporary saved image.
-   
+    """Create a user defined meme."""
     img_url = request.form['image_url']
     img_tmp = f'./tmp/{random.randint(0, 100000000)}.jpg'
     img_content = requests.get(img_url, stream=True).content
     with open(img_tmp, 'wb') as f:
         f.write(img_content)
-    
+
     if request.method == 'POST':
         body = request.form.get('body')
         author = request.form.get('author')
