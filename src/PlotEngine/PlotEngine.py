f84aa55014eb15966d5cf23d5a940fead8879ea4
Jonathan Yeh <johnnyyeh0723@gmail.com>
2021-08-24 23:08:56 +0800

    Revise Plot Engine module. Add argparse function.

--- src/PlotEngine/PlotEngine.py
+++ src/PlotEngine/PlotEngine.py
@@ -1,47 +1,34 @@
 """Import modules for data visualization."""
-import os
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-
-
-photos_dir = '_data/photos/picked/'
-photos_list = os.listdir(photos_dir)
-
-dog_photos = []
-cat_photos = []
-view_photos = []
-for p in photos_list:
-    if p.startswith('x'):
-        dog_photos.append(p)
-    elif p.startswith('n'):
-        cat_photos.append(p)
-    else:
-        view_photos.append(p)
-
-cat_photos = sorted(cat_photos)
-dog_photos = sorted(dog_photos)
-view_photos = sorted(view_photos)
-new_photos_list = dog_photos + cat_photos + view_photos
 
 
 class PlotEngine:
     """Define PlotEngine class."""
 
+    photos_dir = '_data/photos/picked/'
+
     @classmethod
-    def plot(cls) -> str:
+    def plot(cls, category) -> str:
         """To show all images used to generate meme."""
         fig = plt.figure()
-        fig.set_size_inches(16, 12)
-        nrows = 3
-        ncols = 4
 
-        for i in range(0, len(new_photos_list)):
+        if len(category) == 12:
+            fig.set_size_inches(16, 12)
+            nrows = 3
+            ncols = 4
+        else:
+            fig.set_size_inches(4, 3)
+            nrows = 1
+            ncols = 4
+
+        for i in range(0, len(category)):
             ax = plt.subplot(nrows, ncols, i+1)
             ax.axis('Off')
-            img_name = new_photos_list[i]
+            img_name = category[i]
             ax.set_title(img_name, fontsize=10)
-            img = mpimg.imread(photos_dir + img_name)
+            img = mpimg.imread(cls.photos_dir + img_name)
             plt.imshow(img)
         plt.show()
 
-        return "Data visualization for images: {}".format(new_photos_list)
+        return f"Data visualization for images: {category}"
--- src/PlotEngine_test.py
+++ src/PlotEngine_test.py
@@ -1,4 +1,56 @@
 """Import PlotEngine."""
 from PlotEngine import PlotEngine
+import argparse
+import os
 
-print(PlotEngine.plot())
+
+def get_photos():
+    """To get all photos for data visualization."""
+    photos_dir = '_data/photos/picked/'
+    photos_list = os.listdir(photos_dir)
+
+    dog_photos = []
+    cat_photos = []
+    view_photos = []
+    for p in photos_list:
+        if p.startswith('x'):
+            dog_photos.append(p)
+        elif p.startswith('n'):
+            cat_photos.append(p)
+        else:
+            view_photos.append(p)
+
+    cat_photos = sorted(cat_photos)
+    dog_photos = sorted(dog_photos)
+    view_photos = sorted(view_photos)
+    all_photos = dog_photos + cat_photos + view_photos
+
+    return cat_photos, dog_photos, view_photos, all_photos
+
+
+def args_get():
+    """To use argparse to have console accept user input."""
+    parser = argparse.ArgumentParser("Parse a photo set to view data")
+    parser.add_argument("-ps", "--photo_set", required=True,
+                        type=str, help="The name of photo set")
+    args = parser.parse_args()
+
+    return args
+
+
+def plot_decision(cat_photos, dog_photos, view_photos, all_photos, args):
+    """To decide a plot category."""
+    if args.photo_set == 'cat_photos':
+        print(PlotEngine.plot(cat_photos))
+    elif args.photo_set == 'dog_photos':
+        print(PlotEngine.plot(dog_photos))
+    elif args.photo_set == 'view_photos':
+        print(PlotEngine.plot(view_photos))
+    else:
+        print(PlotEngine.plot(all_photos))
+
+
+if __name__ == "__main__":
+    cat_photos, dog_photos, view_photos, all_photos = get_photos()
+    args = args_get()
+    plot_decision(cat_photos, dog_photos, view_photos, all_photos, args)
